14e25741c2a62c795bef9e44bef22881ad703378
Sebastian Garrido <hersega32@gmail.com>
2026-04-29 19:39:35 -0400

    feat: add transaction API service layer

---
+++ services/api/transactions.ts
@@ -0,0 +1,37 @@
+import apiClient from './apiClient';
+import { Transaction, SummaryResponse, IncomeExpenseData, DistributionData } from '../../types/transaction';
+
+export const getSummary = async (): Promise<SummaryResponse> => {
+  const response = await apiClient.get('/transactions/summary');
+  return response.data;
+};
+
+export const getTransactionsHistory = async (): Promise<Transaction[]> => {
+  const response = await apiClient.get('/transactions/individual');
+  return response.data;
+};
+
+export const getIncomeVsExpenses = async (): Promise<IncomeExpenseData> => {
+  const response = await apiClient.get('/transactions/analytics/income-vs-expenses');
+  return response.data;
+};
+
+export const getDistribution = async (): Promise<DistributionData[]> => {
+  const response = await apiClient.get('/transactions/analytics/distribution');
+  return response.data;
+};
+
+export const createTransaction = async (data: Omit<Transaction, 'id'>): Promise<Transaction> => {
+  const response = await apiClient.post('/transactions', data);
+  return response.data;
+};
+
+export const updateTransaction = async (id: string, data: Partial<Transaction>): Promise<Transaction> => {
+  const response = await apiClient.put(`/transactions/${id}`, data);
+  return response.data;
+};
+
+export const deleteTransaction = async (id: string): Promise<void> => {
+  const response = await apiClient.delete(`/transactions/${id}`);
+  return response.data;
+};
