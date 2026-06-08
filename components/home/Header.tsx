0238b249975adc02052f3ef8798fd27bdaf0037a
AgustinTroncoso <a.troncoso09@ufromail.cl>
2026-06-08 12:08:40 -0400

    Revert "Reapply "Merge branch 'feature/fixesandroid' into feature/CI-CD""
    This reverts commit 4b974c750ad465514ae1248a6b1f4588b45727d9.

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -29,7 +29,7 @@ export default function Header({ userName = "Usuario" }) {
     <View style={styles.container}>
       <TouchableOpacity
         style={styles.profileSection}
-        onPress={() => router.push("/(tabs)/profile")}
+        onPress={() => router.push("/profile")}
       >
         <View style={styles.avatar}>
           <Image 
